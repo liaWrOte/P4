933770dd43f84f9df5d568082c7d3dd6fdb83b80
liaWrOte <liaworkingonline@gmail.com>
2023-11-07 12:57:20 +0100

    Add github link

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './index.scss';
 
-
-import linkedIn from '../../assets/images/linkedIn.svg'
-import download from '../../assets/images/download.svg'
+import linkedIn from '../../assets/images/linkedIn.svg';
+import download from '../../assets/images/download.svg';
+import github from '../../assets/images/github.svg';
 
 const Intro = () => {
   return (
@@ -13,18 +13,25 @@ const Intro = () => {
         <p className="name">
           Sandrine M'ZE
         </p>
-        <p>Développeuse<br/>front-end</p>
+        <p>Développeuse <br/>front-end</p>
         <strong>créative</strong>
         </h1>
         <div className='intro-links'>
-          <a href="https://www.linkedin.com/in/sandrinemze/">
-            <img src={linkedIn} alt="Lien vers LinkedIn" target="_blank"/>
-          </a>
-          <span>|</span>
-          <a href="">
-            <span>Mon CV</span>
-            <img src={download} alt="Télécharger mon CV" target="_blank"/>
-          </a>
+          <div>
+            <a href="https://www.linkedin.com/in/sandrinemze/">
+              <img src={linkedIn} alt="Lien vers LinkedIn" target="_blank"/>
+            </a>
+            <a href="https://github.com/liaWrOte">
+              <img src={github} alt="Lien vers Github" target="_blank"/>
+            </a>
+          <span className='separator'>|</span>
+          </div>
+          <div>
+            <a href="">
+              <span>CV</span>
+              <img src={download} alt="Télécharger mon CV" target="_blank"/>
+            </a>
+          </div>
         </div>
       </div>
     </div>
