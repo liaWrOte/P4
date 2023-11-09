15c4a5106bd47c23978c832797e5cc6acce6c441
liaWrOte <liaworkingonline@gmail.com>
2023-11-09 16:55:49 +0100

    centering loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -13,20 +13,16 @@ const Loader = () => {
 
   return (
     <div id="spinner">
-
-        {/* <div id="loader"> */}
-        
+        <div id="loader">
             <div id="loading">
                 <div className="circle" id="first"></div>
                 <div className="circle"  id="second"></div>
                 <div className="circle"  id="third"></div>
             </div>
-            
             {haveLoadingText && 
               <span>CHARGEMENT</span>
             }
-        
-        {/* </div> */}
+        </div>
     </div>
   );
 };
